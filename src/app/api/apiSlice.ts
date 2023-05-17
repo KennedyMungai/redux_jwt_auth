9c9bdb9abd2099ff4415d88279db969a520ffaff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 15:17:02 +0300

    Created the baseQuery

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,2 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { logOut, setCredentials } from '../../features/auth/authSlice'
+
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: 'http://localhost:8000/api',
+	credentials: 'include',
+	prepareHeaders: (headers, { getState }) => {
+		const token = getState().auth.token
+
+		if (token) {
+			headers.set('authorization', `Bearer ${token}`)
+		}
+
+		return headers
+	}
+})
