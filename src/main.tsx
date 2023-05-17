8b1779b5738698163dcca3bbb3dcc050420ae464
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 15:50:46 +0300

    Reorganized the imports for the component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { Provider } from 'react-redux'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import App from './App.tsx'
 import store from './app/store.ts'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
