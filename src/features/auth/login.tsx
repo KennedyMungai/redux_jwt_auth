de0145966e3716679e4fd43f17719ce9d4537215
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 16:23:01 +0300

    Same changes

--- src/features/auth/login.tsx
+++ src/features/auth/login.tsx
@@ -8,11 +8,10 @@ import { useLoginMutation } from './authApiSlice'
 
 
 const Login = () => {
-    const userRef = useRef()
-	const errRef = useRef()
+    const userRef = useRef<HTMLInputElement>()
+	const errRef = useRef<HTMLParagraphElement>()
 	const [user, setUser] = useState('')
-    const [pwd, setPwd] = useState('')
-	const [first, setFirst] = useState('')
+	const [pwd, setPwd] = useState('')
 	const [errMsg, setErrMsg] = useState('')
 	const navigate = useNavigate()
 
@@ -26,8 +25,8 @@ const Login = () => {
 	useEffect(() => {
 		setErrMsg('')
 	}, [user, pwd])
-    
-    const handleSubmit = async (e) => {
+
+	const handleSubmit = async (e) => {
 		e.preventDefault()
 
 		try {
@@ -54,7 +53,46 @@ const Login = () => {
 	const handleUserInput = (e) => setUser(e.target.value)
 	const handlePwdInput = (e) => setPwd(e.target.value)
 
-	return <div>Login</div>
+	const content = isLoading ? (
+		<h1>Loading...</h1>
+	) : (
+		<section className='login'>
+			<p
+				ref={errRef}
+				className={errMsg ? 'errmsg' : 'offscreen'}
+				aria-live='assertive'
+			>
+				{errMsg}
+			</p>
+
+			<h1>Employee Login</h1>
+
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='username'>Username:</label>
+				<input
+					type='text'
+					id='username'
+					ref={userRef}
+					value={user}
+					onChange={handleUserInput}
+					autoComplete='off'
+					required
+				/>
+
+				<label htmlFor='password'>Password:</label>
+				<input
+					type='password'
+					id='password'
+					onChange={handlePwdInput}
+					value={pwd}
+					required
+				/>
+				<button>Sign In</button>
+			</form>
+		</section>
+	)
+
+	return content
 }
 
 export default Login
