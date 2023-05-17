a500597f94d3a8b50d97e44c3d540df644b325cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 16:23:26 +0300

    The imports were reoganized

--- src/features/auth/login.tsx
+++ src/features/auth/login.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { setCredentials } from './authSlice'
 import { useLoginMutation } from './authApiSlice'
+import { setCredentials } from './authSlice'
 
 
 
