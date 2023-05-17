b0521d5e92393ea2d9d4da7f5c85a44754ee0a0a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 16:28:32 +0300

    Created an instance of useLocation called location

--- src/features/auth/RequireAuth.tsx
+++ src/features/auth/RequireAuth.tsx
@@ -3,7 +3,8 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { selectCurrentToken } from './authSlice'
 
 const RequireAuth = () => {
-	const token = useSelector(selectCurrentToken)
+    const token = useSelector(selectCurrentToken)
+	const location = useLocation()
 
 	return <div>RequireAuth</div>
 }
