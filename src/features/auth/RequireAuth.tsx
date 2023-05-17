cac5d6b999f17be245263ec03fc499dbc1779a11
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 16:27:55 +0300

    Selected the token from the state manager:

--- src/features/auth/RequireAuth.tsx
+++ src/features/auth/RequireAuth.tsx
@@ -3,6 +3,8 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { selectCurrentToken } from './authSlice'
 
 const RequireAuth = () => {
+	const token = useSelector(selectCurrentToken)
+
 	return <div>RequireAuth</div>
 }
 
