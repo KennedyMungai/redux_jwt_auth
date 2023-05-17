6d32bb0e5e517e80f85cfd26508d8a43f21268b6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 16:26:01 +0300

    Created the RequireAuth component and imported some components from react-router-dom

---
+++ src/features/auth/RequireAuth.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+
+type Props = {}
+
+const RequireAuth = (props: Props) => {
+	return <div>RequireAuth</div>
+}
+
+export default RequireAuth
