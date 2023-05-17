844436aef89b17491d7374bb47e9e5653a4f7a7c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 16:09:28 +0300

    Set the error messag to blank of either the user or the password change

--- src/features/auth/login.tsx
+++ src/features/auth/login.tsx
@@ -11,18 +11,23 @@ const Login = () => {
     const userRef = useRef()
 	const errRef = useRef()
 	const [user, setUser] = useState('')
+    const [pwd, setPwd] = useState('')
 	const [first, setFirst] = useState('')
 	const [errMsg, setErrMsg] = useState('')
-    const navigate = useNavigate()
-    
-    const [login, { isLoading }] = useLoginMutation()
-    const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const [login, { isLoading }] = useLoginMutation()
+	const dispatch = useDispatch()
 
 	useEffect(() => {
 		userRef.current.focus()
 	}, [])
+
+	useEffect(() => {
+		setErrMsg('')
+	}, [user, pwd])
     
-    
+
 	return <div>Login</div>
 }
 
