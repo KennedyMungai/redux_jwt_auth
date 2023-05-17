310118896c9afc98a8f64adbbcc4f1dce2de16a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 15:31:49 +0300

    Added the apiSlice reducer to teh reducer object

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+import { apiSlice } from './api/apiSlice'
 
 const store = configureStore({
 	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
 		auth: authReducer
 	}
 })
