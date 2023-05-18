485c1694e180ea437bbb4667b2338476bb852156
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-18 08:29:49 +0300

    Defined the routing for the front end application

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,20 @@ import Welcome from './features/auth/Welcome'
 import RequireAuth from './features/auth/RequireAuth'
 
 function App() {
-	return <p>Hello</p>
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				{/* Public Routes */}
+				<Route index element={<Public />} />
+				<Route path='/login' element={<Login />} />
+
+				{/* Protected Routes */}
+				<Route element={<RequireAuth />}>
+					<Route path='/welcome' element={<Welcome />} />
+				</Route>
+			</Route>
+		</Routes>
+	)
 }
 
 export default App
