48f697bc182d1038e02e026dbf7b48b754c8c4e4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-18 08:25:45 +0300

    Added imports for the react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/login'
+import Welcome from './features/auth/Welcome'
+import RequireAuth from './features/auth/RequireAuth'
+
 function App() {
 	return <p>Hello</p>
 }
