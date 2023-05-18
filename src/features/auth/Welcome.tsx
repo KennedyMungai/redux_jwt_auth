2e8e2b126953bfc5382353795423d2d477bafe27
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-18 08:18:44 +0300

    Added imports for the component

--- src/features/auth/Welcome.tsx
+++ src/features/auth/Welcome.tsx
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom'
+import { useAppSelector } from '../../app/hooks'
+import { selectCurrentToken, selectCurrentUser } from './authSlice'
+
 const Welcome = () => {
 	return <div>Welcome</div>
 }
